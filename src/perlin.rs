416d42b6aaeced9d84c1bcccf16810be65bb9c2d
Drew Youngwerth <drew@youngwerth.com>
2019-04-23 11:04:48 -0600

    Perlin WIP cont

--- src/perlin.rs
+++ src/perlin.rs
@@ -1,5 +1,5 @@
 use super::vec3::Vec3;
-use rand::prelude::*;
+use rand::{prelude::*, seq::SliceRandom};
 
 #[derive(Copy, Clone)]
 pub struct Perlin {
@@ -20,13 +20,41 @@ impl Perlin {
     }
 
     pub fn noise(&self, p: Vec3) -> f32 {
-        // let u = p.x - p.x.floor();
-        // let v = p.y - p.y.floor();
-        // let w = p.z - p.z.floor();
-        let i = (4.0 * p.x) as usize & 255;
-        let j = (4.0 * p.y) as usize & 255;
-        let k = (4.0 * p.z) as usize & 255;
-        self.rand_float[(self.x[i] ^ self.y[j] ^ self.z[k]) as usize]
+        let u = p.x - p.x.floor();
+        let v = p.y - p.y.floor();
+        let w = p.z - p.z.floor();
+        let i = p.x.floor();
+        let j = p.y.floor();
+        let k = p.z.floor();
+        let mut c: [[[f32; 2]; 2]; 2] = [[[0.0; 2]; 2]; 2];
+        for di in 0..2 {
+            for dj in 0..2 {
+                for dk in 0..2 {
+                    let x_index = ((i as i32 + di as i32) & 255) as usize;
+                    let y_index = ((j as i32 + dj as i32) & 255) as usize;
+                    let z_index = ((k as i32 + dk as i32) & 255) as usize;
+                    let rand_float_index =
+                        (self.x[x_index] ^ self.y[y_index] ^ self.z[z_index]) as usize;
+                    c[di][dj][dk] = self.rand_float[rand_float_index];
+                }
+            }
+        }
+        Self::trilinear_interp(&c, u, v, w)
+    }
+
+    fn trilinear_interp(c: &[[[f32; 2]; 2]; 2], u: f32, v: f32, w: f32) -> f32 {
+        let mut accum = 0.0;
+        for i in 0..2 {
+            for j in 0..2 {
+                for k in 0..2 {
+                    accum += (i as f32 * u + (1.0 - i as f32) * (1.0 - u))
+                        * (j as f32 * v + (1.0 - j as f32) * (1.0 - v))
+                        * (k as f32 * w + (1.0 - k as f32) * (1.0 - w))
+                        * c[i][j][k];
+                }
+            }
+        }
+        accum
     }
 
     fn gen_float_list() -> [f32; 256] {
@@ -40,12 +68,7 @@ impl Perlin {
 
     fn permute(p: &mut [i32; 256]) {
         let mut rng = thread_rng();
-        for i in 0..p.len() {
-            let target = (rng.gen::<f32>() * i as f32) as usize;
-            let tmp = p[i];
-            p[i] = p[target];
-            p[target] = tmp;
-        }
+        p.shuffle(&mut rng);
     }
 
     fn gen_int_list() -> [i32; 256] {
