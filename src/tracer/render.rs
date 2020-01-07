d8648452478ea2cdb49a1eb03a073e2f4b1bd3c1
Drew Youngwerth <drew@youngwerth.com>
2020-01-07 11:21:15 -0700

    Render cleanup

--- src/tracer/render.rs
+++ src/tracer/render.rs
@@ -10,6 +10,8 @@ use indicatif::{ProgressBar, ProgressStyle};
 use num_cpus;
 use std::thread;
 
+type RenderThread = thread::JoinHandle<Vec<u8>>;
+
 fn color(r: &Ray, world: &impl Hitable, depth: i32) -> Vec3 {
     let rec = match world.hit(r, 0.001, MAX) {
         Some(rec) => rec,
@@ -54,11 +56,11 @@ fn sample_color(scene: &Scene, i: i32, j: i32) -> Vec3 {
     col.sqrt()
 }
 
-fn render_section(scene: Scene, starty: i32, endy: i32, pb: ProgressBar) -> Vec<u8> {
-    let mut file: Vec<u8> = Vec::with_capacity((endy - starty) as usize * scene.nx as usize * 3);
+fn render_section(scene: Scene, start_y: i32, end_y: i32, pb: ProgressBar) -> Vec<u8> {
+    let mut file: Vec<u8> = Vec::with_capacity((start_y - end_y) as usize * scene.nx as usize * 3);
     let Scene { nx, .. } = scene;
 
-    for j in (starty..endy).rev() {
+    for j in (end_y..start_y).rev() {
         for i in 0..nx {
             let col = sample_color(&scene, i, j);
             file.push((255.99 * col.x).max(0.0).min(255.0) as u8);
@@ -70,7 +72,7 @@ fn render_section(scene: Scene, starty: i32, endy: i32, pb: ProgressBar) -> Vec<
     file
 }
 
-fn render_progress_bar(ny: i32) -> ProgressBar {
+fn make_render_progress_bar(ny: i32) -> ProgressBar {
     let pb = ProgressBar::new(ny as u64);
     pb.set_style(
         ProgressStyle::default_bar()
@@ -82,49 +84,55 @@ fn render_progress_bar(ny: i32) -> ProgressBar {
     pb
 }
 
+fn spawn_render_thread(scene: &Scene, pb: &ProgressBar, start_y: i32, end_y: i32) -> RenderThread {
+    let thread_scene = scene.clone();
+    let thread_pb = pb.clone();
+    thread::spawn(move || render_section(thread_scene, start_y, end_y, thread_pb))
+}
+
+fn join_render_threads_into_file(threads: Vec<RenderThread>, file: &mut Vec<u8>) {
+    for render_thread in threads {
+        file.extend(render_thread.join().unwrap());
+    }
+}
+
+/// Generate y sections of in image as (start_y, end_y). Always counts downwards.
+fn generate_y_sections(ny: i32, section_size: i32) -> Vec<(i32, i32)> {
+    let mut start_y = ny;
+    let mut end_y = start_y - section_size;
+    let mut vec: Vec<(i32, i32)> = Vec::new();
+
+    while end_y >= 0 {
+        vec.push((start_y, end_y));
+        start_y = end_y;
+        end_y -= section_size;
+    }
+
+    vec
+}
+
 pub fn render(scene: Scene, path: String) {
     let Scene { nx, ny, .. } = scene;
     let mut file: Vec<u8> = Vec::with_capacity((nx as usize) * (ny as usize) * 3);
-
+    let pb = make_render_progress_bar(ny);
     let thread_count = num_cpus::get();
-    let mut render_threads: Vec<thread::JoinHandle<Vec<u8>>> = Vec::with_capacity(thread_count);
     let y_section_size = ny / thread_count as i32;
-    let mut start_y = ny - y_section_size;
-    let mut end_y = ny;
-    let pb = render_progress_bar(ny);
 
-    for _thread in 0..thread_count {
-        let thread_scene = scene.clone();
-        let thread_pb = pb.clone();
-        let render_thread =
-            thread::spawn(move || render_section(thread_scene, start_y, end_y, thread_pb));
-        render_threads.push(render_thread);
-        end_y = start_y;
-        start_y -= y_section_size;
+    let mut render_threads: Vec<RenderThread> = Vec::with_capacity(thread_count);
+    for (start_y, end_y) in generate_y_sections(ny, y_section_size) {
+        render_threads.push(spawn_render_thread(&scene, &pb, start_y, end_y));
     }
+    join_render_threads_into_file(render_threads, &mut file);
 
-    for render_thread in render_threads {
-        file.extend(render_thread.join().unwrap());
-    }
-
-    // Render remaining y columns
-    let remaining_y_columns = ny - (y_section_size * thread_count as i32);
+    let remaining_y_columns = ny % y_section_size;
     if remaining_y_columns > 0 {
         render_threads = Vec::with_capacity(remaining_y_columns as usize);
         for column in (0..remaining_y_columns).rev() {
-            let thread_scene = scene.clone();
-            let thread_pb = pb.clone();
-            let render_thread =
-                thread::spawn(move || render_section(thread_scene, column - 1, column, thread_pb));
-            render_threads.push(render_thread);
+            render_threads.push(spawn_render_thread(&scene, &pb, column + 1, column));
         }
 
-        for render_thread in render_threads {
-            file.extend(render_thread.join().unwrap());
-        }
+        join_render_threads_into_file(render_threads, &mut file);
     }
-
-    pb.finish_with_message("Complete");
 
     match image::save_buffer(path, &file, nx as u32, ny as u32, image::ColorType::RGB(8)) {
         Err(e) => {
@@ -133,6 +141,8 @@ pub fn render(scene: Scene, path: String) {
         }
         _ => {}
     }
+
+    pb.finish_with_message("Complete");
 }
 
 pub fn render_animation(scene: AnimatedScene, path: String) {
